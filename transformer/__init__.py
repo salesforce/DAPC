69c85278ecff955dbdc187a925d6d87b6d9e7cd1
Yingbo <yingbo.zhou@salesforce.com>
2021-03-02 17:26:34 -0800

    add license

--- transformer/__init__.py
+++ transformer/__init__.py
@@ -3,4 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 
+# Salesforce Research (Junwen Bai, Weiran Wang)
+
 # Left empty for now.
